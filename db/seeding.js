84b59aae592e15a8af4c418270fdd5acae8a6edb
Ikram Syawal Alitu <ikramsyawalalitu@gmail.com>
2024-03-01 16:05:03 +0800

    fix require path for queries.js resulting in error

--- db/seeding.js
+++ db/seeding.js
@@ -1,4 +1,4 @@
-const pool = require("./db/queries");
+const pool = require("./queries.js");
 const fs = require("fs");
 
 const seedQuery = fs.readFileSync("./db/seeding.sql", (encoding = "utf-8"));
