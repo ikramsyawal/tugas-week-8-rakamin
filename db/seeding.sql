ced60d94e78d9c2f7de898af12b32f320044e34f
Ikram Syawal Alitu <ikramsyawalalitu@gmail.com>
2024-03-01 15:54:34 +0800

    creating a query to insert 5 data into actor table

---
+++ db/seeding.sql
@@ -0,0 +1,7 @@
+INSERT INTO actor (actor_id, first_name, last_name, last_update)
+VALUES
+  (201, 'Elijah', 'Wood', '2018-04-22T14:36:40.221Z'),
+  (202, 'Zooey', 'Deschanel', '2019-09-17T10:25:55.987Z'),
+  (203, 'Dev', 'Patel', '2020-11-30T18:47:12.554Z'),
+  (204, 'Felicity', 'Jones', '2021-06-08T07:12:33.789Z'),
+  (205, 'Riz', 'Ahmed', '2022-02-14T22:01:05.366Z');
