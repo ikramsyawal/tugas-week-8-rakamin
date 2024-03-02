d60111b1351aa38b4c4e215055dfa04efc6fdffe
Ikram Syawal Alitu <ikramsyawalalitu@gmail.com>
2024-03-02 14:14:27 +0800

    Add dynamic SQL query to GET route handler

--- router.js
+++ router.js
@@ -1,16 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const pool = require("./db/queries");
 
-router.get("/", (req, res) => {
-  res.send("Hello World");
-});
+router.get("/:nama/:id?", (req, res) => {
+  const nama = req.params.nama;
+  const id = req.params.id;
+  let query = "";
 
-router.post("/", (req, res) => {
-  res.send("Hello World from post");
-});
+  if (nama != "film_category") {
+    query = `SELECT * FROM ${nama} ORDER BY ${nama + "_id"} ASC`;
 
-router.get("/about", (req, res) => {
-  res.send("About Us");
+    if (id) {
+      query = `SELECT * FROM ${nama} WHERE ${nama + "_id"} = ${id}`;
+    }
+  } else {
+    query = `SELECT * FROM film_category`;
+
+    if (id) {
+      query = `SELECT category.name, category.category_id, film.title, film.film_id  FROM film_category INNER JOIN category ON film_category.category_id = category.category_id INNER JOIN film ON film_category.film_id = film.film_id WHERE film_category.category_id = ${id}`;
+    }
+  }
+
+  pool.query(query, (err, result) => {
+    if (err) {
+      throw err;
+    } else {
+      res.send(result.rows);
+    }
+  });
 });
 
 module.exports = router;
